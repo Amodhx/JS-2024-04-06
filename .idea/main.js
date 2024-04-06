1d219a0f69da260d462a8e0934d2ba4515495395
Amodhx <amxdhnanditha@gmail.com>
2024-04-06 10:11:50 +0530

    2

--- .idea/main.js
+++ .idea/main.js
@@ -14,3 +14,21 @@ if (marks >= 75){
 
 var x = (marks >= 75) ? "A" : (marks>=65) ? "B" : (marks >= 55) ? "C" : (marks >=35) ? "S" :"F";
 console.log(x)
+
+
+/*for (let i = 0; i < 101; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}*/
+
+var amo = 0 ;
+while (true){
+    if (amo % 2 == 0 ){
+        console.log(amo)
+    }
+    if (amo == 100){
+        break;
+    }
+    amo++;
+}
